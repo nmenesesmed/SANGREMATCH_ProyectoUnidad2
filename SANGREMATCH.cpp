1102714fea59b3b112589f01650d40b906fe2227
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-08-04 21:36:01 -0500

    parte 1
    Ingreso de las bibliotecas a usar en el proyecto y creación de la estructura Persona

---
+++ SANGREMATCH.cpp
@@ -0,0 +1,11 @@
+#include <iostream>     // Incluye la entrada y salida estándar
+#include <string>       // Incluye el manejo de cadenas de texto
+#include <iomanip>      // Incluye el manejo de formatos de entrada/salida
+
+using namespace std;    // Evitar el uso de std:: continuamente
+
+// Estructura para almacenar la información de una persona
+struct Persona {
+    string nombre;       // Nombre de la persona
+    string tipo_sangre;  // Tipo de sangre de la persona
+};
